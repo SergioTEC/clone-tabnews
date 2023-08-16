26b7d0807797139e5aa1046ddeb34b20c67c82b1
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2023-08-15 23:33:13 -0300

    add scripts `lint:check` and `lint:fix`

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1448cfcf8027459e"; }
+/******/ 		__webpack_require__.h = function() { return "eb98253201b21b1d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementation of a clone of https://www.tabnews.com.br/
